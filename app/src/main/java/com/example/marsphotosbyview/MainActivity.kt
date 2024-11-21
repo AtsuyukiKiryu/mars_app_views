271483ed123d45bf37b96dbf4eaaa897676894e9
AtsuyukiKiryu <atsuyuki.kiryu@festil.jp>
2024-11-21 18:44:30 +0900

    不要コメント削除

--- app/src/main/java/com/example/marsphotosbyview/MainActivity.kt
+++ app/src/main/java/com/example/marsphotosbyview/MainActivity.kt
@@ -25,46 +25,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-
-
-//        // ImageViewを取得
-//        val imageView: ImageView = findViewById(R.id.imageView2)
-//
-//        // Glideを使用して画像をロード
-        val imageUrl = "https://ccsrpcml.carsensor.net/CSphoto/ml/026/842/UZ0047026842/UZ0047026842_13_001.jpg"
-//        Glide.with(this).load(imageUrl).into(imageView)
-
-
-//
-//        val testImageView = findViewById<ImageView>(R.id.testImageView)
-//        Glide.with(this)
-//            .load(imageUrl) // テスト用の画像URL
-//            .into(testImageView)
-
-
-// テキストデータのリストを作成
-//        val items = List(20) { "Item #$it" } // "Item #0", "Item #1", ...
-//
-//        recyclerView = findViewById(R.id.recyclerView)
-//        recyclerView.layoutManager = GridLayoutManager(this, 2)
-//        recyclerView.adapter = TextAdapter(items)
-
-
-//         URLリストを作成
-//        val imageUrls = listOf(
-//            "https://ccsrpcml.carsensor.net/CSphoto/ml/026/842/UZ0047026842/UZ0047026842_13_001.jpg",
-//            "https://ccsrpcml.carsensor.net/CSphoto/ml/026/842/UZ0047026842/UZ0047026842_13_001.jpg",
-//            "https://ccsrpcml.carsensor.net/CSphoto/ml/026/842/UZ0047026842/UZ0047026842_13_001.jpg",
-//            "https://ccsrpcml.carsensor.net/CSphoto/ml/026/842/UZ0047026842/UZ0047026842_13_001.jpg",
-//        )
-//        recyclerView =    findViewById(R.id.recyclerView)
-//        recyclerView.layoutManager = GridLayoutManager(this, 2) // 2列のグリッド
-//        recyclerView.adapter = ImageAdapter(imageUrls)
 
         val topAppBar: MaterialToolbar = findViewById(R.id.topAppBar)
         topAppBar.setNavigationOnClickListener {
